d36f6f80b5995bf80b35b3499d57f1bc71fc3999
Nathan Blackburn <nblackburndeveloper@icloud.com>
2025-07-23 07:07:58 +0100

    move all .cpp content into .h

--- include/synapse/details/concurrenct_hash_map.h
+++ include/synapse/details/concurrenct_hash_map.h
@@ -5,6 +5,8 @@
 #include <unordered_map>
 #include <functional>
 #include <shared_mutex>
+#include <mutex>
+#include <optional>
 
 template<typename Key, typename Value, typename Hash = std::hash<Key>>
 class ConcurrentHashMap
@@ -12,24 +14,51 @@ class ConcurrentHashMap
 private:
     struct Shard
     {
-        std::unordered_map hash_map;
+        std::unordered_map<Key, Value, Hash> hash_map;
         mutable std::shared_mutex mutex_;
     } 
-    std::vector<Shard> shards;
 
-    ConcurrentHashMap() {}
+    Shard& get_shard(const Key& key) 
+    {
+        return shards.at(hasher(key) % shards.size());
+    }
+
+    const Shard& get_shard(const Key& key) const 
+    {
+        return shards.at(hasher(key) % shards.size());
+    }
+
+    Hash hasher;
+    std::vector<Shard> shards;
+public:
+    ConcurrentHashMap(size_t shard_count = 32) : shards(shard_count) {}
     ~ConcurrentHashMap() {}
 
     ConcurrentHashMap(const ConcurrentHashMap&) = delete;
     ConcurrentHashMap operator=(ConcurrentHashMap&) = delete;
-public:
-    void insert(const Key& key, const Value& val);
-    const Value& get(const Key& key) const;
-    const Value& erase(const Key& key) const;
+
+    void insert(const Key& key, const Value& val)
+    {
+        auto& shard = get_shard(key);
+        std::unique_lock lock(shard.mutex_);
+        shard.hash_map[key] = value;
+    }
+
+    std::optional<Value> get(const Key& key) const
+    {
+        const auto& shard = get_shard(key);
+        std::shared_lock lock(shard.mutex_);
+        if (auto it = shard.hash_map.find(key); it != shard.hash_map.end())
+            return it->second;
+        return std::nullptr;
+    }
+
+    const Value& erase(const Key& key) 
+    {
+        auto& shard = get_shard(key);
+        std::unique_lock lock(shard.mutex_);
+        shard.hash_map.erase(key);
+    }
 }
 
 #endif  // CONCURRENT_HASH_MAP_H
-
-
-// update gemini to tell me which chapter/section of the book to read before doing a feature
-// + maybe explain i need to learn rather than it doing it for me 
